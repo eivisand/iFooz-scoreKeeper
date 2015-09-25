e3f1ae3d4eb4de3bee399472c7a2b7132aecbb83
eivisand <eivind sandstad@mac-eivindsandstad.bekk.no>
2015-09-25 14:51:00 +0200

    big-red-button functions shamelessly added into init

--- keepScore.js
+++ keepScore.js
@@ -4,3 +4,33 @@ var score = {
   blue: 0,
 }
 
+var init = function() {
+  var bigRedButtons = [];
+  for (var i = 0; i < BigRedButton.deviceCount(); i++) {
+    console.log('opening BigRedButton', i);
+
+    bigRedButtons.push(new BigRedButton.BigRedButton(i));
+
+    bigRedButtons[i].on('buttonPressed', function () {
+      console.log('Button pressed');
+      console.log(bigRedButtons[i])
+    });
+
+    bigRedButtons[i].on('buttonReleased', function () {
+      console.log('Button released');
+    });
+
+    bigRedButtons[i].on('lidRaised', function () {
+      console.log('Lid raised');
+    });
+
+    bigRedButtons[i].on('lidClosed', function () {
+      console.log('Lid closed');
+    });
+
+  }
+  console.log(bigRedButtons);
+
+}
+
+module.exports = init;
